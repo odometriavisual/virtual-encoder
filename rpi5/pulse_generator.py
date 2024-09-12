df394925b9114bc01bd43c1dd60774a1cf32bb1e
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-09-12 14:05:25 -0300

    develop class PulseGenerator

--- rpi5/pulse_generator.py
+++ rpi5/pulse_generator.py
@@ -1,7 +1,27 @@
+import RPi.GPIO as gpio
+
 class PulseGenerator:
+    sequenceArray = [[gpio.HIGH, gpio.HIGH], [gpio.HIGH, gpio.LOW], [gpio.LOW, gpio.LOW], [gpio.LOW, gpio.HIGH]]
+
     def __init__(self, PIN_A: int, PIN_B: int):
-        raise NotImplemented
+        self.PIN_A = PIN_A
+        self.PIN_B = PIN_B
+        self.positionReference = 0
+
+        gpio.setup(self.PIN_A, gpio.OUT)
+        gpio.setup(self.PIN_B, gpio.OUT)
 
     def send_pulses(self, count: int):
-        for i in range(count):
-            print(f"send_pulse: {i}")
+        if count > 0:
+            pulseDirectionMultiplier = 1
+        else:
+            pulseDirectionMultiplier = -1
+
+        absoluteCount = abs(count)
+
+        for i in range(absoluteCount):
+            self.positionReference = (self.positionReference + pulseDirectionMultiplier)%4
+
+            gpio.output(self.PIN_A,self.sequenceArray[self.positionReference][0])
+            gpio.output(self.PIN_B,self.sequenceArray[self.positionReference][1])
+

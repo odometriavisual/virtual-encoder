a16a033f98122ad235ffcc3bfb609b4e26b2f1e4
Thiago Kalid <thiagokkalid@gmail.com>
2024-09-13 17:41:40 -0300

    fix server host address

--- rpi5/pi_zero_client.py
+++ rpi5/pi_zero_client.py
@@ -3,19 +3,19 @@ from numpy import integer
 import cv2
 import requests
 
-PIZERO_HOST = 'http://10.42.0.95:7123/'
+PIZERO_HOST = 'http://raspberrypi00.local:7123/'
 
 class PiZeroClient:
     video_stream = None
 
-    def set_focus(self, focus: integer):
-        requests.get(f'http://raspberrypi00.local:7123/focus.html/{focus}')
+    def set_focus(self, focus: int):
+        requests.get(f'{PIZERO_HOST}/focus.html/{focus}')
 
-    def set_exposure(self, exposure: integer):
-        requests.get(f'http://raspberrypi00.local:7123/exposure.html/{exposure}')
+    def set_exposure(self, exposure: int):
+        requests.get(f'{PIZERO_HOST}/exposure.html/{exposure}')
 
     def get_orientation(self) -> [float, float, float, float]:
-        return requests.get(f'http://raspberrypi00.local:7123/imu.html').text.strip().split(',')
+        return requests.get(f'{PIZERO_HOST}/imu.html').text.strip().split(',')
 
     def get_mjpeg_stream(self) -> cv2.VideoCapture:
         return cv2.VideoCapture(f'{PIZERO_HOST}/stream.mjpg')

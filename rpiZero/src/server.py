603503208c012ba10ddc3d01b2102a6db698ba54
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-09-30 16:17:58 -0300

    Develop of some tools to generate a chessboard

--- rpiZero/src/server.py
+++ rpiZero/src/server.py
@@ -120,23 +120,7 @@ class Server:
                 return 0
 
         def _find_corners(self, img):
-            # Color-segmentation to get binary mask
-            lwr = np.array([0, 0, 143])
-            upr = np.array([179, 61, 252])
-            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            msk = cv2.inRange(hsv, lwr, upr)
-
-            # Extract chess-board
-            krn = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 30))
-            dlt = cv2.dilate(msk, krn, iterations=5)
-            res = 255 - cv2.bitwise_and(dlt, msk)
-
-            # Displaying chess-board features
-            res = np.uint8(res)
-            ret, corners = cv2.findChessboardCorners(res, (8, 8),
-                                                     flags=cv2.CALIB_CB_ADAPTIVE_THRESH +
-                                                           cv2.CALIB_CB_FAST_CHECK +
-                                                           cv2.CALIB_CB_NORMALIZE_IMAGE)
+            ret, corners = cv2.findChessboardCorners(img, (7, 7), flags=cv2.CALIB_CB_ADAPTIVE_THRESH
             return ret, corners
 
         def _stream_video(self):

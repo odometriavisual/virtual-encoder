bb23e5651125c68f95ec9d717bbc6493b623f9d6
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-09-30 16:06:31 -0300

    Develop of some tools to generate a chessboard

--- rpiZero/src/server.py
+++ rpiZero/src/server.py
@@ -135,7 +135,7 @@ class Server:
             ret, corners = cv2.findChessboardCorners(res, (7, 7),
                                                      flags=cv2.CALIB_CB_ADAPTIVE_THRESH +
                                                            cv2.CALIB_CB_FAST_CHECK +
-                                                           cv2.CALIB_CB_NORMALIZE_IMAGE
+                                                           cv2.CALIB_CB_NORMALIZE_IMAGE)
             return ret, corners
 
         def _stream_video(self):

a8c7cefab65165003a1e82673fc8024396fdca37
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-09-30 16:51:25 -0300

    Develop of some tools to generate a chessboard

--- rpiZero/src/server.py
+++ rpiZero/src/server.py
@@ -123,9 +123,9 @@ class Server:
             img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             temp = cv2.GaussianBlur(img_gray, (0, 0), 105)
-            cv2.addWeighted(img_gray, 1.8, temp, -0.8, 0, img_gray)
+            img_gray = cv2.addWeighted(img_gray, 1.8, temp, -0.8, 0, img_gray)
 
-            ret1, corners = cv2.findChessboardCorners(img_gray, (5, 5), flags=cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)
+            ret1, corners = cv2.findChessboardCorners(img_gray, (6, 6), flags=cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)
             ret2, corners = cv2.findChessboardCorners(img_gray, (7, 7), flags=cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)
             print(corners)
             ret3, corners = cv2.findChessboardCorners(img_gray, (8, 8), flags=cv2.CALIB_CB_ADAPTIVE_THRESH | cv2.CALIB_CB_NORMALIZE_IMAGE)

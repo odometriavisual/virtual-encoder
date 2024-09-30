4ea81f1e423369a8195cd20dba15b06f59345991
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-09-30 13:31:07 -0300

    bugfix, image is on format BRG

--- rpiZero/src/localPiZeroClient.py
+++ rpiZero/src/localPiZeroClient.py
@@ -15,7 +15,7 @@ class LocalPiZeroClient:
         # Inicializa a câmera
         self.picam2 = Picamera2()
 
-        self.picam2.configure(self.picam2.create_preview_configuration({'format': 'BGR888'}))
+        self.picam2.configure(self.picam2.create_preview_configuration({'format': 'RGB888'}))
         self.picam2.configure(self.picam2.create_video_configuration(main={"size": (640, 480)}))
 
         self.picam2.controls.FrameRate = 60  # Ajusta o FPS

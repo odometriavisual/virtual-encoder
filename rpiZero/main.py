ed7f7703746695df23db8fba47e6be0c16a3e271
Daniel Santin <danielsantin@alunos.utfpr.edu.br>
2024-09-30 14:34:06 -0300

    Calibration tests

--- rpiZero/main.py
+++ rpiZero/main.py
@@ -2,13 +2,23 @@ from src.localPiZeroClient import LocalPiZeroClient
 from src.server import Server
 import time
 from src.localCalibration import startLocalCalibration
-
+import cv2
 
 def main():
     client = LocalPiZeroClient()
     time.sleep(1)
 
     startLocalCalibration(client, 4, 6, 0.1)
+    img = client.get_img()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray)
+
+    if ret:
+        point1 = corners[0][0]  # Ponto no topo à esquerda
+        point2 = corners[6][0]  # Ponto no topo à direita
+        print(point1)
+        print(point2)
+
 
     server = Server(client)
     server.run()

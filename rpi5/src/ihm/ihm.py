0e56a53199044eb23c12cf14cbf4323ed1dc8425
Fernando <caetanof@alunos.utfpr.edu.br>
2024-10-18 13:21:24 -0300

    cache status as a dict on rpi5

--- rpi5/src/ihm/ihm.py
+++ rpi5/src/ihm/ihm.py
@@ -12,20 +12,26 @@ class IHM:
         self.estado: str = ''
         self.ip: str = ''
 
-        self.camera_status: str  = ''
-        self.imu_status: str  = ''
-        self.rpiZero_status: str  = ''
+        self.status: dict = {
+            'rpiZero': False,
+            'imu': False,
+            'camera': False,
+        }
 
     def send_event(self, ev):
         self._event_queue.put(ev)
 
     def update_display(self):
+        camera = 'Ok' if self.status['camera'] else 'Err.'
+        rpi_zero = 'Ok' if self.status['rpiZero'] else 'Err.'
+        imu = 'Ok' if self.status['imu'] else 'Err.'
+
         self.oled_screen.drawLine(0, f'MODO: {self.modo}', 'center')
         self.oled_screen.drawLine(1, f'ESTADO: {self.estado}', 'center')
 
         self.oled_screen.drawLine(2, f'IP: {self.ip}')
-        self.oled_screen.drawLine(3, f'CAM : {self.camera_status}')
-        self.oled_screen.drawLine(4, f'piZ: {self.rpiZero_status} | IMU: {self.imu_status}')
+        self.oled_screen.drawLine(3, f'CAM : {camera}')
+        self.oled_screen.drawLine(4, f'piZ: {rpi_zero} | IMU: {imu}')
 
         self.oled_screen.update()
 

d45e31b00749ad2e13991b4b093200d610cd36a3
Thiago Kalid <thiagokkalid@gmail.com>
2024-10-21 18:01:19 -0300

    Fix to update calibration main to work according to new architecture

--- rpi0/main_calibration.py
+++ rpi0/main_calibration.py
@@ -2,9 +2,22 @@ from src.localPiZeroClient import LocalPiZeroClient
 import time
 from src.localCalibration import startLocalCalibration
 from src.calibration.calibration_server import CalibrationServer
+from picamera2 import Picamera2
+import adafruit_bno055
+
 
 def main():
-    client = LocalPiZeroClient()
+    picam = Picamera2()
+    picam.configure(picam.create_preview_configuration({'format': 'RGB888', "size": (640, 480)}))
+    picam.controls.FrameRate = 60  # Ajusta o FPS
+    picam.start()
+
+    try:
+        imu = adafruit_bno055.BNO055_I2C(board.I2C(), 0x29)
+    except :
+        imu = None
+
+    client = LocalPiZeroClient(picam, imu)
     time.sleep(1)
 
     server = CalibrationServer(client)

af1be3ab5dca57e0f2735149df43d6303efb6734
Fernando <caetanof@alunos.utfpr.edu.br>
2024-10-22 14:07:00 -0300

    update main calibration

--- rpi0/main_calibration.py
+++ rpi0/main_calibration.py
@@ -1,10 +1,23 @@
-from src.localPiZeroClient import LocalPiZeroClient
+from picamera2 import Picamera2
 import time
-from src.localCalibration import startLocalCalibration
+import adafruit_bno055
+import board
+
+from src.localPiZeroClient import LocalPiZeroClient
 from src.calibration.calibration_server import CalibrationServer
 
 def main():
-    client = LocalPiZeroClient()
+    picam = Picamera2()
+    picam.configure(picam.create_preview_configuration({'format': 'RGB888', "size": (640, 480)}))
+    picam.controls.FrameRate = 60  # Ajusta o FPS
+    picam.start()
+
+    try:
+        imu = adafruit_bno055.BNO055_I2C(board.I2C(), 0x29)
+    except :
+        imu = None
+
+    client = LocalPiZeroClient(picam, imu)
     time.sleep(1)
 
     server = CalibrationServer(client)

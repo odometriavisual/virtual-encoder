b4366d231354f32cdb43380c84447bd89d2cd270
Thiago Kalid <thiagokkalid@gmail.com>
2024-10-24 15:11:58 -0300

    create object responsible for handling external usb hhd/ssd

---
+++ rpi5/src/mount_device_manager.py
@@ -0,0 +1,38 @@
+import time
+import subprocess
+from subprocess import SubprocessError
+
+class MountDeviceManager:
+    def __init__(self, device="/dev/sda1", mount_point="/media/usb-ssd"):
+        self.device = device
+        self.mount_path = mount_point
+
+    def is_mounted(self):
+        return subprocess.run(["findmnt", "-S", self.device, "-T", self.mount_path]).returncode == 0
+
+    def mount(self) -> bool:
+        try:
+            if not self.is_mounted():
+                subprocess.run(["sudo", "mount", self.device])
+
+            time.sleep(.5)
+            return True
+        except SubprocessError:
+            return False
+
+    def unmount(self) -> bool:
+        try:
+            time.sleep(.5)
+            subprocess.run(["sudo", "unmount", self.device])
+            return True
+        except SubprocessError:
+            return False
+ 
+if __name__ == "__main__":
+    print("Before mounting")
+    ssd = MountDeviceManager(device="/dev/sda1", mount_point="/media/usb-ssd")
+
+    print(f'ssd.mount() = {ssd.mount()}')
+    print(f'Mounted: {ssd.is_mounted()}')
+    #print("ssd.unmount() = ",ssd.unmount())
+    #print("Unmounted")

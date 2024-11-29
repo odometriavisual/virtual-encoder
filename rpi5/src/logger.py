0fc97f61655611ae1883b9d83e1256eec40ce094
Thiago Kalid <thiagokkalid@gmail.com>
2024-11-29 13:28:34 -0300

    finished the logger class for rpi5 displacement

--- rpi5/src/logger.py
+++ rpi5/src/logger.py
@@ -1,63 +1,65 @@
 from os import makedirs
 from datetime import datetime
 from os.path import isfile, isdir
-from .modos.modo_odometro import ModoOdometro
+from collections import deque
 
 import threading, csv, time
 
 class Logger:
     def __init__(self, modo):
+         # Read the boot number:
         with open('/home/pi/boot-count.txt') as file:
             self.boot_num = file.read().strip()
 
+        self.path = '/dev/null'
+        self.save_dir = None
+        self.active = False
+        self.queue = deque()
+        self.thread = threading.Thread(target=self._save_displacement, daemon=True)
+        
+    def _create_newdir(self):
         time_now = time.time_ns()
-        datenow = datetime.fromtimestamp(time_now // 1_000_000_000).strftime('%Y%m%dT%H%M%S')
-        self.save_dir = f'/home/pi/picam_imgs/{self.boot_num}_{datenow}'
-        self.modo = modo
+        self.datenow = datetime.fromtimestamp(time_now // 1_000_000_000).strftime('%Y%m%dT%H%M%S')
+        self.save_dir = f'/home/pi/picam_imgs/{self.boot_num}_{self.datenow}'
+        if not isdir(self.save_dir):
+            makedirs(self.save_dir)
+    
+    def start(self):
+        self._create_newdir()
+        self.active = True
 
-        self.enable_save = False
+        # Check if there is a log file, create otherwise 
+        self.path = f'{self.save_dir}/displacement.csv'
+        if not isfile(self.path):
+            with open(self.path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["timestamp", "x", "y"])
 
-    def _poll_modo(self):
-        time.sleep(10)
-        while True:
-            time.sleep(0.1)
-            time_now = time.time_ns()
-            if isinstance(self.modo, ModoOdometro):
-                if not self.enable_save:
-                    datenow = datetime.fromtimestamp(time_now // 1_000_000_000).strftime('%Y%m%dT%H%M%S')
-                    self.save_dir = f'/home/pi/picam_imgs/{self.boot_num}_{datenow}'
-                    if not isdir(self.save_dir):
-                        makedirs(self.save_dir)
-                        self._save_displacement()
 
-                    self.enable_save = True
-            elif self.enable_save:
-                self.enable_save = False
+        self.thread.start()
 
+    def reset(self):
+        self.active = False
+
+    def log(self, displacement):
+        self.queue.append(displacement)
 
     def _save_displacement(self):
-        last_displacement_index = 0
         while True:
-            time.sleep(0.1)
-            if self.enable_save:
-                path = f'{self.save_dir}/imu.csv'
-                if not isfile(path):
-                    with open(path, mode='w', newline='') as file:
-                        writer = csv.writer(file)
-                        writer.writerow(["timestamp", "x", "y"])
+            time.sleep(0.01)
+          
+            # Check if queue is not empty:
+            if len(self.queue) != 0:
+                current_displacement = self.queue.pop()
+                time_now = time.time_ns()
+                with open(self.path, mode='a', newline='') as file:
+                    writer = csv.writer(file)
+                    row = [time_now, current_displacement[0], current_displacement[1]]
+                    writer.writerow(row)
+            else:
+                if not self.active:
+                    self.path = '/dev/null'
+                    self.save_dir= None
+                    break
 
-                current_displacement_index = self.modo.odometer.number_of_displacements
-                if current_displacement_index != last_displacement_index and current_displacement_index != 0:
-                    time_now = time.time_ns()
-                    with open(path, mode='a', newline='') as file:
-                        writer = csv.writer(file)
-                        row = [time_now, self.modo.odometer[0], self.modo.odometer[1]]
-                        writer.writerow(row)
-                    last_displacement_index = current_displacement_index
-                elif current_displacement_index == 0:
-                    last_displacement_index = 0 # resets the index
-
-    def start(self):
-        threading.Thread(target=self._poll_modo, daemon=True).start()
-        threading.Thread(target=self._save_displacement, daemon=True).start()
 

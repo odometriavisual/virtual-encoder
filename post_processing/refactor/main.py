62b6a3b652a2814d784b52b1634fe84e4cb972bf
Demarky <danielsantin@alunos.utfpr.edu.br>
2025-03-20 14:40:02 -0300

    Bugfix

--- post_processing/refactor/main.py
+++ post_processing/refactor/main.py
@@ -3,5 +3,5 @@ from plot_2d import plot2DFromData
 from plot_3d import plot3DFromData
 
 loaded_data = processFolder()
-plot2DFromData(loaded_data["displacements"])
 plot3DFromData(loaded_data["displacements"],loaded_data["quaternions"])
+plot2DFromData(loaded_data["displacements"])

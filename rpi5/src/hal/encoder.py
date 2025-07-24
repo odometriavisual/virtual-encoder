db88b5f4c972f88b13bf523d5da5475da06a73b2
Fernando <caetanof@alunos.utfpr.edu.br>
2025-07-24 16:25:08 -0300

    add EncoderGPIO

--- rpi5/src/hal/encoder.py
+++ rpi5/src/hal/encoder.py
@@ -1,3 +1,49 @@
+import gpiod
+from gpiod.line import Direction, Value
+
+
 class EncoderNull:
-    def send_pulse(self, direction: str = "+"):
+    def send_pulse(self, direction="+"):
         pass
+
+
+class EncoderGPIO(EncoderNull):
+    def __init__(self, PIN_A: int, PIN_B: int, chip: str = "/dev/gpiochip4"):
+        super().__init__()
+
+        self.sequenceArray = [
+            [Value.ACTIVE, Value.ACTIVE],
+            [Value.ACTIVE, Value.INACTIVE],
+            [Value.INACTIVE, Value.INACTIVE],
+            [Value.INACTIVE, Value.ACTIVE],
+        ]
+        self.positionReference = 0
+        self.PIN_A = PIN_A
+        self.PIN_B = PIN_B
+        self.LINES = gpiod.request_lines(
+            chip,
+            consumer="pulse-generator",
+            config={
+                self.PIN_A: gpiod.LineSettings(
+                    direction=Direction.OUTPUT,
+                    output_value=self.sequenceArray[self.positionReference][0],
+                ),
+                self.PIN_B: gpiod.LineSettings(
+                    direction=Direction.OUTPUT,
+                    output_value=self.sequenceArray[self.positionReference][1],
+                ),
+            },
+        )
+
+        self.LINES.set_value(self.PIN_A, self.sequenceArray[self.positionReference][0])
+        self.LINES.set_value(self.PIN_B, self.sequenceArray[self.positionReference][1])
+
+    def send_pulse(self, direction="+"):
+        if direction == "+":
+            pulseDirectionMultiplier = 1
+        else:
+            pulseDirectionMultiplier = -1
+
+        self.positionReference = (self.positionReference + pulseDirectionMultiplier) % 4
+        self.LINES.set_value(self.PIN_A, self.sequenceArray[self.positionReference][0])
+        self.LINES.set_value(self.PIN_B, self.sequenceArray[self.positionReference][1])

551cd4d6fe6af36076bba58b1ccb14c81aded949
Fernando <caetanof@alunos.utfpr.edu.br>
2025-07-24 17:10:13 -0300

    add RelayGPIO

--- rpi5/src/hal/relay.py
+++ rpi5/src/hal/relay.py
@@ -1,6 +1,33 @@
+import gpiod
+from gpiod.line import Direction, Value
+
+
 class RelayNull:
     def turn_on(self):
         pass
 
     def turn_off(self):
         pass
+
+
+class RelayGPIO(RelayNull):
+    def __init__(self, PIN: int = 25, chip: str = "/dev/gpiochip4"):
+        super().__init__()
+
+        self.PIN = PIN
+        self.LINE = gpiod.request_lines(
+            chip,
+            consumer="relay",
+            config={
+                self.PIN: gpiod.LineSettings(
+                    direction=Direction.OUTPUT, output_value=Value.INACTIVE
+                )
+            },
+        )
+
+    # Observação, o rpi0 foi colocado na conexão NF (normalmente fechado), por isso a lógica de ligar e desligar deve ser invertida
+    def turn_on(self):
+        self.LINE.set_value(self.PIN, Value.INACTIVE)
+
+    def turn_off(self):
+        self.LINE.set_value(self.PIN, Value.ACTIVE)

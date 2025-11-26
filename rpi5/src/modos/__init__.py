e9ec8b844ba6b488b2d65c079a6f87fe14d30925
Fernando <caetanof@alunos.utfpr.edu.br>
2025-11-26 15:57:39 -0300

    remove pi zero related commands and ModoDownload

--- rpi5/src/modos/__init__.py
+++ rpi5/src/modos/__init__.py
@@ -1,11 +1,9 @@
 from .modo_tempo import ModoTempo
 from .modo_autonomo import ModoAutonomo
-from .modo_download import ModoDownload
 from .modo_odometro import ModoOdometro
 
 __all__ = [
     "ModoAutonomo",
-    "ModoDownload",
     "ModoTempo",
     "ModoOdometro",
 ]

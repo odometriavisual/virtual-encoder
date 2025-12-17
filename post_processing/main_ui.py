a37314f667548c2d31fe833401c33198bdb11f12
Fernando <caetanof@alunos.utfpr.edu.br>
2025-12-17 15:31:22 -0300

    post_processing: remove post_processing from rpi5 code

--- post_processing/main_ui.py
+++
@@ -1,9 +0,0 @@
-import traceback
-
-if __name__ == "__main__":
-    try:
-        from post_processing.ui.menu_interface import show_main_menu
-        show_main_menu()
-    except Exception as e:
-        print(e)
-        traceback.print_exc()  # Mostra arquivo, linha, função

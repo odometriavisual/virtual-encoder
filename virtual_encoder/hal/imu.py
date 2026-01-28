3df81b418d4b379c7fa79e4da861ee649a1460b3
Fernando <caetanof@alunos.utfpr.edu.br>
2026-01-28 15:11:49 -0300

    imu: fix imu thread not starting

--- virtual_encoder/hal/imu.py
+++ virtual_encoder/hal/imu.py
@@ -12,9 +12,6 @@ class ImuNull:
     def get_orientation(self) -> list[float]:
         return [0, 0, 0, 1]
 
-    def start(self):
-        pass
-
 
 class ImuUDP(ImuNull, threading.Thread):
     def __init__(self, gs: "EncoderGS"):

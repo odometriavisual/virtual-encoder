ea2896b2580222fe9e040360e1611150cd13e946
Fernando <caetanof@alunos.utfpr.edu.br>
2026-01-30 14:14:28 -0300

    display: add status flags and reconnection attempts

--- virtual_encoder/webui/src/status_watcher.js
+++ virtual_encoder/webui/src/status_watcher.js
@@ -31,10 +31,10 @@ export function update_status_watcher(status) {
       Versão ${status.version}
 			Modo ${status.modo}
 			${status.rpi5 === false || status.modo === 'Download'? status.estado:
-        (status.estado === 'Calibrando'? `Calibrando ${status.rpi0.progress}%`: 'Estado' + status.estado)
-    }
+        (status.estado === 'Calibrando'? `Calibrando ${status.rpi0.progress}%`: 'Estado' + status.estado)}
 			${status.rpi5 ? `IP: ${status.rpi5.ip}` : ''}
-			${status.rpi5? `Temp: ${status.rpi5.temp?.toFixed(2)} ℃`: ''}`
+			${status.rpi5? `Temp: ${status.rpi5.temp?.toFixed(2)} ℃`: ''}
+			${status.display? "":"Display não encontrado"}`
 
     window.status_watcher.rpi0.innerText = `RPi Zero
 			${status.rpi0 ? `Temp: ${status.rpi0.temp?.toFixed(2)} ℃` : ''}`

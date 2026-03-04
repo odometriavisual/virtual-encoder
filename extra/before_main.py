e9712aa274df565e2a08bb66b00a3b0a4872fdec
Fernando <caetanof@alunos.utfpr.edu.br>
2026-03-04 14:40:11 -0300

    display scripts: dont crash on error

--- extra/before_main.py
+++ extra/before_main.py
@@ -1,7 +1,10 @@
-from src.hal.display import DisplaySSD1306
+try:
+    from src.hal.display import DisplaySSD1306
 
-if __name__ == "__main__":
-    screen = DisplaySSD1306(None)
-    screen.clear()
-    screen.draw_line(2, "Inicializando...")
-    screen.update()
+    if __name__ == "__main__":
+        screen = DisplaySSD1306(None)
+        screen.clear()
+        screen.draw_line(2, "Inicializando...")
+        screen.update()
+except Exception:
+    pass
--- extra/before_shutdown.py
+++ extra/before_shutdown.py
@@ -1,10 +1,13 @@
-from src.hal.display import DisplaySSD1306
+try:
+    from src.hal.display import DisplaySSD1306
 
-if __name__ == "__main__":
-    screen = DisplaySSD1306(None)
-    screen.clear()
-    screen.draw_line(0, "Desligando...")
-    screen.draw_line(2, "Aguarde 30 segundos")
-    screen.draw_line(3, "antes de desconectar")
-    screen.draw_line(4, "cabos de energia")
-    screen.update()
+    if __name__ == "__main__":
+        screen = DisplaySSD1306(None)
+        screen.clear()
+        screen.draw_line(0, "Desligando...")
+        screen.draw_line(2, "Aguarde 30 segundos")
+        screen.draw_line(3, "antes de desconectar")
+        screen.draw_line(4, "cabos de energia")
+        screen.update()
+except Exception:
+    pass

fix not using average radius

virtual_encoder/modos/modo_calibracao.py
@@ -104,7 +104,7 @@ class ModoCalibracao:
 
 
             if len(radius_found) > 0:
-                self.gs.spatial_resolution = printed_diameter / (2 * radius)
+                self.gs.spatial_resolution = printed_diameter / (2 * np.average(radius_found))
 
                 self.gs.add_message(
                     f"Resolução espacial calibrada para {1/self.gs.spatial_resolution:.3f} px/mm"
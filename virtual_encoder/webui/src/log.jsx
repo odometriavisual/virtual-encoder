3b9a1b600d28a0f0754a1d89a58ee51e4a8a310f
Fernando <caetanof@alunos.utfpr.edu.br>
2026-06-16 13:26:36 -0300

    fix log links

--- virtual_encoder/webui/src/log.jsx
+++ virtual_encoder/webui/src/log.jsx
@@ -1,3 +1,5 @@
+import "./log.css";
+
 import { useEffect, useRef } from 'preact/hooks'
 import { TrajectoryGraph } from './trajectory_graph';
 import { useEncoder } from './encoder_context';
@@ -34,8 +36,8 @@ export function Log() {
           :
           null
       }
-      <div class="log-window">
-        {log.map(line => <div class="log-line">{line}</div>)}
+      <div class="log-window" >
+        {log.map(line => <div class="log-line" dangerouslySetInnerHTML={{__html: line}}></div>)}
       </div>
       <button onClick={() => set_log([])}> Apagar log </button>
     </div>

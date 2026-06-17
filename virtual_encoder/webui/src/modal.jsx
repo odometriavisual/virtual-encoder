a593d99773137e3f4f47213dab1dd893dc85cf3f
Fernando <caetanof@alunos.utfpr.edu.br>
2026-06-17 13:59:04 -0300

    build webui and fix typo

--- virtual_encoder/webui/src/modal.jsx
+++ virtual_encoder/webui/src/modal.jsx
@@ -177,7 +177,7 @@ function ModalCalibracao() {
   const [dist_mm, set_dist_mm] = useState(1);
 
   const r = Math.sqrt(status.pos.x * status.pos.x + status.pos.y * status.pos.y).toFixed(2);
-  const spatial_res = abs(r / dist_mm);
+  const spatial_res = Math.abs(r / dist_mm);
 
   const calibrate_photo = () => {
     encoder_api.calibrate_resolution("photo", 8);

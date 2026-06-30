0a5059677e72c0d8f6223071f3f6e4e1aaf47a10
Fernando <caetanof@alunos.utfpr.edu.br>
2026-06-30 16:26:13 -0300

    trajectory graph: add grid lines

--- frontend/src/trajectory_graph.jsx
+++ frontend/src/trajectory_graph.jsx
@@ -1,3 +1,5 @@
+import "./trajectory_graph.css"
+
 import * as d3 from "d3";
 import { useEffect, useRef } from "preact/hooks";
 import { useEncoder } from "./encoder_context";
@@ -18,14 +20,23 @@ export function TrajectoryGraph({ parent_ref }) {
   }, [status.pos.x, status.pos.y, status.pos.sr])
 
   useEffect(() => {
+    let w = parent_ref.current.clientWidth;
+    let h = parent_ref.current.clientHeight;
+
     const svg = d3.select(svg_ref.current);
 
-    const x = d3.scaleLinear();
-    const y = d3.scaleLinear();
+    const x = d3.scaleLinear().domain([-100 * w / h, 100 * w / h]).range([0, w]);
+    const y = d3.scaleLinear().domain([-100, 100]).range([0, h]);
 
     const build_svg_line = d3.line()
       .x(ps => x(ps.x))
       .y(ps => y(ps.y));
+
+    const x_axis = d3.axisBottom(x).ticks(w/120, ".0f").tickSize(h).tickPadding(8 - h);
+    const y_axis = d3.axisRight(y).ticks(h/120, ".0f").tickSize(w).tickPadding(8 - w);
+
+    const x_g = svg.append("g").attr("color", "#BBB").call(x_axis);
+    const y_g = svg.append("g").attr("color", "#BBB").call(y_axis);
 
     const center_g = svg.append("g");
 
@@ -37,25 +48,37 @@ export function TrajectoryGraph({ parent_ref }) {
 
     const zoom = d3.zoom()
       .on("zoom", ({ transform }) => {
+        center_g.attr("transform", transform);
         path.attr("stroke-width", 2 / transform.k);
 
-        const cx = parent_ref.current.clientWidth / 2;
-        const cy = parent_ref.current.clientHeight / 2;
-        center_g.attr("transform", transform.translate(cx, cy));
+        x_g.call(x_axis.ticks(w/120).scale(transform.rescaleX(x)));
+        y_g.call(y_axis.ticks(h/120).scale(transform.rescaleY(y)));
       });
 
-    const cx = parent_ref.current.clientWidth / 2;
-    const cy = parent_ref.current.clientHeight / 2;
+    svg.call(zoom);
 
-    svg.call(zoom)
-      .call(zoom.transform, d3.zoomIdentity.translate(cx, cy).scale(0.05));
+    update_path_ref.current = points => path.attr("d", build_svg_line(points));
 
-    update_path_ref.current = points => {
-      path.attr("d", build_svg_line(points));
+    const on_resize = () => {
+      w = parent_ref.current.clientWidth;
+      h = parent_ref.current.clientHeight;
+
+      x.domain([-100 * w / h, 100 * w / h]).range([0, w]);
+      y.domain([-100, 100]).range([0, h]);
+
+      x_axis.ticks(w/120, ".0f").tickSize(h).tickPadding(8 - h);
+      y_axis.ticks(h/120, ".0f").tickSize(w).tickPadding(8 - w);
+
+      x_g.call(x_axis);
+      y_g.call(y_axis);
     };
+
+    window.addEventListener("resize", on_resize);
 
     return () => {
       svg_ref.current.innerHTML = "";
+      update_path_ref.current = null;
+      window.removeEventListener("resize", on_resize);
     };
   }, []);
 
